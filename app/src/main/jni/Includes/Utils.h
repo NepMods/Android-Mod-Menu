0dcf2a039fafd60fff0b3bf27fb470fb08b36ae9
NepMods <88999135+NepMods@users.noreply.github.com>
2022-04-25 11:54:51 +0545

    Update Utils.h

--- app/src/main/jni/Includes/Utils.h
+++ app/src/main/jni/Includes/Utils.h
@@ -92,6 +92,102 @@ uintptr_t string2Offset(const char *c) {
     return strtoull(c, nullptr, base);
 }
 
+void T(JNIEnv *env, jobject thiz, const char *text, int length) {
+    jstring jstr = env->NewStringUTF(text);
+    jclass toast = env->FindClass("android/widget/Toast");
+    jmethodID methodMakeText = env->GetStaticMethodID( toast, "makeText", "(Landroid/content/Context;Ljava/lang/CharSequence;I)Landroid/widget/Toast;");
+    if (methodMakeText == NULL) { return;  }
+    jobject toastobj = env->CallStaticObjectMethod(toast, methodMakeText, thiz, jstr, length);
+     jmethodID methodShow = env->GetMethodID(toast, "show", "()V");
+    if (methodShow == NULL) {  return; }
+    env->CallVoidMethod(toastobj, methodShow);
+}
+struct {
+     int L = 1;
+      int S = 0;
+} Length;
+void D(jobject ctx, JNIEnv *env, const char *title, const char *msg){
+    jclass Alert = env->FindClass("android/app/AlertDialog$Builder");
+    jmethodID AlertCons = env->GetMethodID(Alert, "<init>", "(Landroid/content/Context;)V");
+
+    jobject MainAlert = env->NewObject(Alert, AlertCons, ctx);
+
+    jmethodID setTitle = env->GetMethodID(Alert, "setTitle", "(Ljava/lang/CharSequence;)Landroid/app/AlertDialog$Builder;");
+    env->CallObjectMethod(MainAlert, setTitle, env->NewStringUTF(title));
+
+    jmethodID setMsg = env->GetMethodID(Alert, ("setMessage"), ("(Ljava/lang/CharSequence;)Landroid/app/AlertDialog$Builder;"));
+    env->CallObjectMethod(MainAlert, setMsg, env->NewStringUTF(msg));
+
+    jmethodID setCa = env->GetMethodID(Alert, ("setCancelable"), ("(Z)Landroid/app/AlertDialog$Builder;"));
+    env->CallObjectMethod(MainAlert, setCa, false);
+
+    jmethodID setPB = env->GetMethodID(Alert, ("setPositiveButton"), ("(Ljava/lang/CharSequence;Landroid/content/DialogInterface$OnClickListener;)Landroid/app/AlertDialog$Builder;"));
+    env->CallObjectMethod(MainAlert, setPB, env->NewStringUTF("Ok"), static_cast<jobject>(NULL));
+
+    jmethodID create = env->GetMethodID(Alert, ("create"), ("()Landroid/app/AlertDialog;"));
+    jobject creaetob = env->CallObjectMethod(MainAlert, create);
+
+    jclass AlertN = env->FindClass(("android/app/AlertDialog"));
+
+    jmethodID show = env->GetMethodID(AlertN, ("show"), ("()V"));
+    env->CallVoidMethod(creaetob, show);
+}
+void DFull(jobject ctx, JNIEnv *env, const char *title, const char *msg, const char *Ok,const char *Cancel, bool usCloseable ){
+    jclass Alert = env->FindClass("android/app/AlertDialog$Builder");
+    jmethodID AlertCons = env->GetMethodID(Alert, "<init>", "(Landroid/content/Context;)V");
+
+    jobject MainAlert = env->NewObject(Alert, AlertCons, ctx);
+
+    jmethodID setTitle = env->GetMethodID(Alert, "setTitle", "(Ljava/lang/CharSequence;)Landroid/app/AlertDialog$Builder;");
+    env->CallObjectMethod(MainAlert, setTitle, env->NewStringUTF(title));
+
+    jmethodID setMsg = env->GetMethodID(Alert, ("setMessage"), ("(Ljava/lang/CharSequence;)Landroid/app/AlertDialog$Builder;"));
+    env->CallObjectMethod(MainAlert, setMsg, env->NewStringUTF(msg));
+
+    jmethodID setCa = env->GetMethodID(Alert, ("setCancelable"), ("(Z)Landroid/app/AlertDialog$Builder;"));
+    env->CallObjectMethod(MainAlert, setCa, usCloseable);
+
+    jmethodID setPB = env->GetMethodID(Alert, ("setPositiveButton"), ("(Ljava/lang/CharSequence;Landroid/content/DialogInterface$OnClickListener;)Landroid/app/AlertDialog$Builder;"));
+    env->CallObjectMethod(MainAlert, setPB, env->NewStringUTF(Ok), static_cast<jobject>(NULL));
+
+    jmethodID setNB = env->GetMethodID(Alert, ("setNegativeButton"), ("(Ljava/lang/CharSequence;Landroid/content/DialogInterface$OnClickListener;)Landroid/app/AlertDialog$Builder;"));
+    env->CallObjectMethod(MainAlert, setNB, env->NewStringUTF(Cancel), static_cast<jobject>(NULL));
+
+    
+    jmethodID create = env->GetMethodID(Alert, ("create"), ("()Landroid/app/AlertDialog;"));
+    jobject creaetob = env->CallObjectMethod(MainAlert, create);
+
+    jclass AlertN = env->FindClass(("android/app/AlertDialog"));
+
+    jmethodID show = env->GetMethodID(AlertN, ("show"), ("()V"));
+    env->CallVoidMethod(creaetob, show);
+}
+
+void CP(JNIEnv *env, jobject ctx, const char *perm){
+    jclass native_context = env->GetObjectClass(ctx);
+    jmethodID startActivity = env->GetMethodID(native_context, ("startActivity"),("(Landroid/content/Intent;)V"));
+
+    jmethodID pack = env->GetMethodID(native_context, ("getPackageName"),("()Ljava/lang/String;"));
+    jstring packageName = static_cast<jstring>(env->CallObjectMethod(ctx, pack));
+
+    const char *pkg = env->GetStringUTFChars(packageName, 0);
+
+    std::stringstream pkgg;
+    pkgg << ("package:");
+    pkgg << pkg;
+    std::string pakg = pkgg.str();
+
+    jclass Uri = env->FindClass(("android/net/Uri"));
+    jmethodID Parce = env->GetStaticMethodID(Uri, ("parse"), ("(Ljava/lang/String;)Landroid/net/Uri;"));
+    jobject UriMethod = env->CallStaticObjectMethod(Uri, Parce, env->NewStringUTF(pakg.c_str()));
+
+    jclass intentclass = env->FindClass(("android/content/Intent"));
+    jmethodID newIntent = env->GetMethodID(intentclass, ("<init>"), ("(Ljava/lang/String;Landroid/net/Uri;)V"));
+    jobject intent = env->NewObject(intentclass,newIntent,env->NewStringUTF((perm)), UriMethod);
+
+    env->CallVoidMethod(ctx, startActivity, intent);
+}
+
 namespace ToastLength {
     inline const int LENGTH_LONG = 1;
     inline const int LENGTH_SHORT = 0;
