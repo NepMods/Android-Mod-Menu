8d1affe0a4c3889b354dca337c1e3aca6ac3b896
Abhikr-X | अभिषेक <abhikr6714@gmail.com>
2025-07-06 09:56:07 +0530

    Update BuildConfig.java

--- BuildConfig.java
+++ BuildConfig.java
@@ -1,4 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.android.support;
+
 public final class BuildConfig {
-    public static final boolean INCLUDE_THEME_AZL = false;
-    public static final boolean INCLUDE_THEME_DZ = false;
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.android.support";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "4.0";
+  // Field from default config.
+  public static final boolean INCLUDE_THEME_AZL = false;
+  // Field from default config.
+  public static final boolean INCLUDE_THEME_DZ = false;
 }
