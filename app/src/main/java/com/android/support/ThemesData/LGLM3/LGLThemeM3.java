8ffd7ce9efa0740a215d012b044606b317d5580d
tojik_proof_93 <elementman50@yandex.com>
2025-12-11 11:45:32 +0500

    Change Init method parameters to TextView
    Updated Init method to accept TextView parameters and cast them to MaterialTextView.

--- app/src/main/java/com/android/support/ThemesData/LGLM3/LGLThemeM3.java
+++ app/src/main/java/com/android/support/ThemesData/LGLM3/LGLThemeM3.java
@@ -2,6 +2,7 @@ package com.android.support.ThemesData.LGLM3;
 
 import android.content.Context;
 import android.view.View;
+import android.widget.TextView;
 import com.google.android.material.textview.MaterialTextView;
 
 import com.android.support.Menu;
@@ -52,8 +53,9 @@ public class LGLThemeM3 implements IMenuTheme {
             }
 
             @Override
-            public void Init(Context context, MaterialTextView title, MaterialTextView subTitle) {
-                menuData.Init(context, title, subTitle);
+            public void Init(Context context, TextView title, TextView subTitle) {
+                // Кастуем на MaterialTextView, если уверены, что это безопасно
+            menuData.Init(context, (MaterialTextView) title, (MaterialTextView) subTitle);
             }
         });
         shared.menu.build();
